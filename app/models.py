6dabb91c2920197b8598322908909ed795e9abe9
Siddharth Goutam <116184096+Siddharth-2611@users.noreply.github.com>
2025-07-06 16:18:06 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(20), default="Pending")
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
